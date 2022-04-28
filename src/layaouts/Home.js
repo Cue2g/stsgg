f478779b80bf3c75648a48791d072ca8e3433ca7
Gustavo <Gustavoblancog29@gmail.com>
2022-04-28 13:24:47 -0400

    toyo test

--- src/layaouts/Home.js
+++ src/layaouts/Home.js
@@ -26,25 +26,25 @@ export default function Home() {
     }
   return (
       <div className="uk-position-center">
-         <div className="uk-card uk-card-body uk-margin">
+         <div className="uk-card uk-card-body uk-margin uk-animation-slide-right">
             <img src={require('../img/logo.png')}  alt="logo" width="150px"/>
-            <div className="uk-margin">
+            <div className="uk-margin uk-animation-shake">
                 <div className="uk-inline">
-                    <span className="uk-form-icon" uk-icon="icon: user">:)</span>
+                    <span className="uk-form-icon" uk-icon="icon: user">toyo</span>
                     <input className="uk-input" type="text" id="inputName"/>
                 </div>
             </div>
 
-            <div className="uk-margin">
+            <div className="uk-margin uk-animation-slide-top-medium">
                 <select className="uk-select" id="selectHome">
                     {regionRiot.map( res =>  <option key={res.name} value={res.name}>{res.text}</option>)}
                 </select>
             </div>
 
-            <div className="uk-margin">
+            <div className="uk-margin uk-animation-slide-right-medium">
                 <button className="uk-button uk-button-default uk-button" onClick={getSummoner}>Buscar</button>
             </div>
-           
+
             <div className="uk-margin">
             {encontrado === false ? (<p>no se ha encontrado el summoner</p>) : (null)}
             </div>
--- src/layaouts/Nav.js
+++ src/layaouts/Nav.js
@@ -3,7 +3,7 @@ import './Nav.css'
 
 export default function Nav() {
   return (
-    <nav className="uk-navbar-container" uk-navbar='true'>
+    <nav className="uk-navbar-container uk-animation-slide-top" uk-navbar='true'>
       <div className="uk-navbar-left">
         <ul className="uk-navbar-nav">
             <li className="uk-active"><a href="#!">Inicio</a></li>
